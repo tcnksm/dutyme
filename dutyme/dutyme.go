9643fe67f0e85bcf102f8ccf7053b653e5c7245d
tcnksm <nsd22843@gmail.com>
2016-10-16 17:02:46 +0900

    Add Duty client

---
+++ dutyme/dutyme.go
@@ -0,0 +1,84 @@
+package dutyme
+
+import (
+	"github.com/PagerDuty/go-pagerduty"
+	"github.com/pkg/errors"
+	"github.com/tcnksm/go-gitconfig"
+	"github.com/tcnksm/go-input"
+)
+
+type Dutyme struct {
+	PD PagerDuty
+	UI *input.UI
+}
+
+func (d *Dutyme) GetUser(defaultEmail string) (*pagerduty.APIObject, error) {
+	if len(defaultEmail) == 0 {
+		// PD email address may be same as git email address
+		defaultEmail, _ = gitconfig.Email()
+	}
+
+	query := "Input Pagerduty account email address"
+	email, err := d.UI.Ask(query, &input.Options{
+		Default:   defaultEmail,
+		Required:  true,
+		Loop:      true,
+		HideOrder: true,
+	})
+
+	if err != nil {
+		return nil, errors.Wrap(err, "faield to ask PD email address")
+	}
+
+	return d.PD.GetUser(email)
+}
+
+func (d *Dutyme) GetSchedule(defaultQuery string) (string, string, error) {
+	query := "Input Pagerduty schedule name which you want to override"
+	scheduleQuery, err := d.UI.Ask(query, &input.Options{
+		Default:   defaultQuery,
+		Required:  true,
+		Loop:      true,
+		HideOrder: true,
+	})
+
+	schedules, err := d.PD.GetSchedules(scheduleQuery)
+	if err != nil {
+		return "", "", err
+	}
+
+	var (
+		name string
+		ID   string
+	)
+
+	// API may return multiple schedules. Then ask to user.
+	if len(schedules) > 1 {
+		targets := make([]string, 0, len(schedules))
+		for _, schedule := range schedules {
+			targets = append(targets, schedule.Name)
+		}
+
+		query := "Found multiple schedules. Select one."
+		target, err := d.UI.Select(query, targets, &input.Options{
+			Default: targets[0],
+			Loop:    true,
+		})
+
+		if err != nil {
+			return "", "", errors.Wrap(err, "failed to ask schedule from the given list")
+		}
+
+		for _, schedule := range schedules {
+			if target == schedule.Name {
+				name = schedule.Name
+				ID = schedule.ID
+			}
+		}
+	} else {
+		name = schedules[0].Name
+		ID = schedules[0].ID
+	}
+
+	return name, ID, nil
+}
---
+++ dutyme/dutyme_test.go
@@ -0,0 +1,101 @@
+package dutyme
+
+import (
+	"bytes"
+	"io/ioutil"
+	"testing"
+
+	"github.com/tcnksm/go-input"
+)
+
+func TestDutyme_GetUser(t *testing.T) {
+	email := "cunningham@pagerduty.com"
+	d := Dutyme{
+		UI: &input.UI{
+			Writer: ioutil.Discard,
+			Reader: bytes.NewBufferString(email),
+		},
+		PD: testNewClient(t),
+	}
+
+	defaultEmail := ""
+	user, err := d.GetUser(defaultEmail)
+	if err != nil {
+		t.Fatal("GetUser failed:", err)
+	}
+
+	if got, want := user.ID, "PGJ36Z3"; got != want {
+		t.Fatalf("GetUser: user.ID = %s;  want %s", got, want)
+	}
+}
+
+func TestDutyme_GetUser_default(t *testing.T) {
+	d := Dutyme{
+		UI: &input.UI{
+			Writer: ioutil.Discard,
+			Reader: bytes.NewBufferString("\n"),
+		},
+		PD: testNewClient(t),
+	}
+
+	defaultEmail := "cunningham@pagerduty.com"
+	user, err := d.GetUser(defaultEmail)
+	if err != nil {
+		t.Fatal("GetUser failed:", err)
+	}
+
+	if got, want := user.ID, "PGJ36Z3"; got != want {
+		t.Fatalf("GetUser: user.ID = %s;  want %s", got, want)
+	}
+}
+
+func TestDutyme_GetSchedule(t *testing.T) {
+	d := Dutyme{
+		UI: &input.UI{
+			Writer: ioutil.Discard,
+			Reader: bytes.NewBufferString("BoothDuty\n1\n"),
+		},
+		PD: testNewClient(t),
+	}
+
+	defaultQuery := ""
+	name, id, err := d.GetSchedule(defaultQuery)
+	if err != nil {
+		t.Fatal("GetSchedule failed:", err)
+	}
+
+	if want := "PKM7ZY1"; id != want {
+		t.Fatalf("GetSchedule ID = %q, want %q", id, want)
+	}
+
+	if want := "BoothDuty Primary"; name != want {
+		t.Fatalf("GetSchedule name = %q, want %q", name, want)
+	}
+
+}
+
+func TestDutyme_GetSchedule_withoutAsking(t *testing.T) {
+	t.Skip("TODO: need to fix when query includes space")
+	d := Dutyme{
+		UI: &input.UI{
+			Writer: ioutil.Discard,
+			Reader: bytes.NewBufferString("BoothDuty Primary\n"),
+		},
+		PD: testNewClient(t),
+	}
+
+	defaultQuery := ""
+	name, id, err := d.GetSchedule(defaultQuery)
+	if err != nil {
+		t.Fatal("GetSchedule failed:", err)
+	}
+
+	if want := "PKM7ZY1"; id != want {
+		t.Fatalf("GetSchedule ID = %q, want %q", id, want)
+	}
+
+	if want := "BoothDuty Primary"; name != want {
+		t.Fatalf("GetSchedule name = %q, want %q", name, want)
+	}
+
+}
