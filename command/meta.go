b7ba5ec86d2d72c8bc33d9fa3d364fd042c498bd
tcnksm <nsd22843@gmail.com>
2016-10-18 23:10:21 +0900

    Refactoring and add new flags

--- command/meta.go
+++ command/meta.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"os"
 	"path/filepath"
 
 	homedir "github.com/mitchellh/go-homedir"
@@ -12,10 +13,35 @@ import (
 )
 
 const (
+	EnvToken = "PD_SERVICE_KEY"
+
+	EnvDebug = "DUTYME_DEBUG"
+	EnvTrace = "DUTYME_TRACE"
+)
+
+const (
+	ExitCodeOK = iota
+	ExitCodeError
+)
+
+const (
 	DefaultConfigName = ".dutyme.json"
 )
 
-var Debug bool
+var (
+	Debug bool
+	Trace bool
+)
+
+func init() {
+	if v := os.Getenv(EnvDebug); len(v) != 0 {
+		Debug = true
+	}
+
+	if v := os.Getenv(EnvTrace); len(v) != 0 {
+		Trace = true
+	}
+}
 
 // Meta contain the meta-option that nearly all subcommand inherited.
 type Meta struct {
@@ -43,10 +69,21 @@ func (m *Meta) ConfigPath() (string, error) {
 	return filepath.Join(home, DefaultConfigName), nil
 }
 
-func (m *Meta) AskToken(defaultToken string) (string, error) {
+func (m *Meta) AskToken() (string, error) {
+	fmt.Fprintf(m.OutStream, `To use dutyme command, you need a PagerDuty API v2 token.
+The token must have full access to read, write, update, and delete.
+
+Only account administrators have the ability to generate token.
+(See more about token on official doc https://goo.gl/VPvlwB)
+
+You can set the token via %q env var (then you can
+skip the following input). Or after running first overriding, you can
+save login info on config file.
+
+`, EnvToken)
+
 	query := "Input PagerDuty API token"
 	return m.UI.Ask(query, &input.Options{
-		Default:   defaultToken,
 		Required:  true,
 		Loop:      true,
 		HideOrder: true,
@@ -54,8 +91,18 @@ func (m *Meta) AskToken(defaultToken string) (string, error) {
 	})
 }
 
+// Debugf prints debug information when debug env var
+// has non-empty value. If not, it does nothing.
 func Debugf(format string, args ...interface{}) {
 	if Debug {
 		log.Printf("[DEBUG] "+format+"\n", args...)
+	}
+}
+
+// Trace prints pkg/errors stack trace information when trace env
+// var has non-empty value. If not, it does nothing.
+func TracePrint(w io.Writer, err error) {
+	if Trace {
+		fmt.Fprintf(w, "\n[TRACE] %+v\n", err)
 	}
 }
--- command/start.go
+++ command/start.go
@@ -2,9 +2,7 @@ package command
 
 import (
 	"fmt"
-	"log"
 	"os"
-	"strings"
 	"time"
 
 	"github.com/tcnksm/dutyme/config"
@@ -12,57 +10,118 @@ import (
 	"github.com/tcnksm/go-input"
 )
 
+const (
+	// DefaultWorkingTime is default duration of override.
+	DefaultWorkingTime = 1 * time.Hour
+
+	// TimeFmt is used for displaying time
+	TimeFmt = "2006-01-02 15:04:05-0700"
+)
+
 type StartCommand struct {
 	Meta
 }
 
-const (
-	EnvToken = "PD_SERVICE_KEY"
-)
+func (c *StartCommand) Synopsis() string {
+	return "Override schedule and assign on-call to you"
+}
 
-const (
-	ExitCodeOK = iota
-	ExitCodeError
-)
+func (c *StartCommand) Help() string {
+	helpText := fmt.Sprintf(`Usage: dutyme start [options...]
+
+start overrides the schedule and assign on-call to you. By default,
+it creates 1 hour override (you can change this via -working option).
+
+To use dutyme command, you need a PagerDuty API v2 token.
+The token must have full access to read, write, update, and delete.
+
+Only account administrators have the ability to generate token.
+(See more about token on official doc https://goo.gl/VPvlwB)
+
+You can set the token via %q env var (then you can
+skip the following input). Or after running first overriding, you can
+save login info on config file.
+
+Options:
+
+  -working TIME  Working time (overriding time). By default, it's 1 hour.
+                 TIME can be specified by decimal numbers with a unit suffix,
+                 such "1.5h" or "2h45m". It must be positive value.
+
+  -update        Update existing configuration file. It asks email and
+                 schedule name again.
+
+  -force         Force overriding without confirmation.
+
+`, EnvToken)
+	return helpText
+}
 
 func (c *StartCommand) Run(args []string) int {
+
+	var (
+		force       bool
+		update      bool
+		workingTime time.Duration
+
+		useExisting bool
+	)
+
 	flags := c.Meta.NewFlagSet("start", c.Help())
+
+	flags.BoolVar(&force, "force", false, "")
+	flags.BoolVar(&force, "f", false, "")
+
+	flags.BoolVar(&update, "update", false, "")
+	flags.DurationVar(&workingTime, "working", DefaultWorkingTime, "")
+
 	if err := flags.Parse(args); err != nil {
 		return ExitCodeError
 	}
 
+	// Find configuration file for dutyme.
 	cfgPath, err := c.Meta.ConfigPath()
 	if err != nil {
-		log.Fatal(err)
+		fmt.Fprintf(c.ErrStream, "Failed to read config path: %s\n", err)
+		TracePrint(c.ErrStream, err)
 		return ExitCodeError
 	}
 
 	cfg := &config.Config{}
 	if _, err := os.Stat(cfgPath); err == nil {
+		Debugf("Use existing configuration file: %s", cfgPath)
+
 		var err error
 		cfg, err = config.ParseFile(cfgPath)
 		if err != nil {
-			log.Fatal(err)
+			fmt.Fprintf(c.ErrStream, "Failed to parse configuration file: %s\n", err)
+			TracePrint(c.ErrStream, err)
 			return ExitCodeError
 		}
+		useExisting = true
 	}
 
+	// Override API token via env var if exsits.
 	if v := os.Getenv(EnvToken); len(v) != 0 {
+		Debugf("Read PD API token from env var: %s", v)
 		cfg.Token = v
 	}
 
 	if len(cfg.Token) == 0 {
-		token, err := c.Meta.AskToken("")
+		token, err := c.Meta.AskToken()
 		if err != nil {
-			log.Fatal(err)
+			fmt.Fprintf(c.ErrStream, "Failed to ask API token: %s\n", err)
+			TracePrint(c.ErrStream, err)
 			return ExitCodeError
 		}
 		cfg.Token = token
 	}
 
+	// Construct Dutyme client with PD HTTP client
 	pd, err := dutyme.NewPDClient(cfg.Token)
 	if err != nil {
-		log.Fatal(err)
+		fmt.Fprintf(c.ErrStream, "Failed to create PD HTTP client: %s\n", err)
+		TracePrint(c.ErrStream, err)
 		return ExitCodeError
 	}
 
@@ -71,43 +130,59 @@ func (c *StartCommand) Run(args []string) int {
 		PD: pd,
 	}
 
-	if cfg.User == nil {
+	// When configuration file is not exist (fisrt time to execute or not saved before).
+	// or when -update flag is provided, ask/get user information.
+	if cfg.IsEmpty() || update {
 		user, err := dutyme.GetUser("")
 		if err != nil {
-			log.Fatal(err)
+			fmt.Fprintf(c.ErrStream, "Failed to get PagerDuty user: %s\n", err)
+			TracePrint(c.ErrStream, err)
 			return ExitCodeError
 		}
-
 		cfg.User = user
-	}
 
-	if cfg.ScheduleID == "" {
 		scheduleName, scheduleID, err := dutyme.GetSchedule("")
 		if err != nil {
-			log.Fatal(err)
+			fmt.Fprintf(c.ErrStream, "Failed to get PagerDuty schedule: %s\n", err)
+			TracePrint(c.ErrStream, err)
 			return ExitCodeError
 		}
-
 		cfg.ScheduleName = scheduleName
 		cfg.ScheduleID = scheduleID
 	}
+	Debugf("User: %s", cfg.User.Email)
+	Debugf("Schedule: %s", cfg.ScheduleName)
 
-	// TODO(tcnsm): Enable to set working time via flag
-	DefaultWorkingTime := 1 * time.Hour
+	// Override time: from now to now + working time
 	start := time.Now()
-	end := start.Add(DefaultWorkingTime)
+	end := start.Add(workingTime)
 
-	force := false
-	log.Printf("Override user %q to schedule %q from %s to %s",
-		cfg.User.Email, cfg.ScheduleName, start, end)
+	fmt.Fprintf(c.OutStream, "Override schedule %q (%s) by user %q\n",
+		cfg.ScheduleName, cfg.ScheduleID, cfg.User.Email)
+	fmt.Fprintf(c.OutStream, "from %s to %s\n",
+		start.Format(TimeFmt), end.Format(TimeFmt))
+
 	override, err := dutyme.Override(cfg.ScheduleID, cfg.User, start, end, force)
 	if err != nil {
-		log.Fatal(err)
+		if IsCancel(err) {
+			fmt.Fprintln(c.OutStream, "Override canceled")
+			return ExitCodeError
+		}
+
+		fmt.Fprintf(c.ErrStream, "Failed to override: %s\n", err)
+		TracePrint(c.ErrStream, err)
 		return ExitCodeError
 	}
-	log.Println("Successfuly overrided:", override.ID)
+	fmt.Fprintf(c.OutStream, "Successfuly overrided schedule (%s)\n", override.ID)
 
-	query := "Want to save login info? (you can skip input from next time) [Y/n]"
+	// If it's used exsiting configuration file,
+	// and -update flag is not provided, then skip the following section.
+	if useExisting && !update {
+		return ExitCodeOK
+	}
+
+	// Save override info on file
+	query := "Want to save override info? (you can skip input from next time) [Y/n]"
 	ans, err := c.Meta.UI.Ask(query, &input.Options{
 		Default:     "Y",
 		Loop:        true,
@@ -125,22 +200,21 @@ func (c *StartCommand) Run(args []string) int {
 		return ExitCodeOK
 	}
 
-	indent := true
-	if err := cfg.WriteFile(cfgPath, indent); err != nil {
-		log.Fatal(err)
+	if err := cfg.WriteFile(cfgPath, true); err != nil {
+		fmt.Fprintf(c.ErrStream, "Failed to save file: %s\n", err)
+		TracePrint(c.ErrStream, err)
 		return ExitCodeError
 	}
+	fmt.Fprintf(c.OutStream, "Successfuly saved file (%s)\n", cfgPath)
 
 	return ExitCodeOK
 }
 
-func (c *StartCommand) Synopsis() string {
-	return ""
+type isCancel interface {
+	IsCancel() bool
 }
 
-func (c *StartCommand) Help() string {
-	helpText := `
-
-`
-	return strings.TrimSpace(helpText)
+func IsCancel(err error) bool {
+	e, ok := err.(isCancel)
+	return ok && e.IsCancel()
 }
